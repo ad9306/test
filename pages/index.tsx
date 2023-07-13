d3c382840c64a8d1a49f09828116bf3e3efe1cc0
ad9306 <elyoon3004@gmail.com>
2023-07-13 18:16:05 +0900

    230713_Head_p_br_olul

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,18 +10,19 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Jey do it</title>
       </Head>
       <>
 
 
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '30vh' }}>
 
           <h1 className={styles.title}>
 
             Slow and steady wins the race.
 
           </h1>
+        
           <h2>
             <Link href="/posts/test_post">
               click this!
@@ -34,6 +35,15 @@ export default function Home() {
             </Link>
           </h2>
         </div>
+          <h1>
+            이렇게 하면<br></br> 어떻게 될까?
+            문단분리 안하면 그냥 쓰이는거고?
+            <p>
+              근데 이렇게 하면 문단 분리라는데 맞나 이거
+            </p>
+            여기 밑에 또 쓰면?
+          </h1>
+          
 
       </>
     </>
--- pages/posts/test_post.tsx
+++ pages/posts/test_post.tsx
@@ -1,13 +1,29 @@
-import Link from "next/link";
+import Head from 'next/head'
+import Link from 'next/link';
+
 
 export default function Ftest_post() {
     return (
         <>
+        <Head>
+            <title>
+                test memo
+            </title>
+        </Head>
             <h1>                 
                 <Link href="/">
                     Home
                 </Link> 
+
             </h1>
+            <h2>
+
+            </h2>
+                <ol type="1">
+                    <li>can</li>
+                    <li>glass</li>
+                    <li>wood</li>
+                </ol>
         </>
     )
 }
