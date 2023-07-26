b91dc27c5bdbcfc431f54593bd5f4ead1dc60eda
ad9306 <90166883+ad9306@users.noreply.github.com>
2023-07-26 12:48:06 +0900

    alert5

--- pages/index.tsx
+++ pages/index.tsx
@@ -100,7 +100,7 @@ export default function Home() {
             Number === "20" && <Link href="/posts/test_post2"><button>드디어</button></Link>
           }
           {
-            Number === "-7" ? clickAlert
+            Number === "-7" ? clickAlert : undefined
           }
         </div>
         <div>
