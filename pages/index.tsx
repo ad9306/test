f8d446de5602b9ab925125c3708f86c22e341319
ad9306 <elyoon3004@gmail.com>
2023-08-11 05:52:32 +0900

    check5

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,23 +6,16 @@ import Link from 'next/link'
 import { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
-
+//1차 정리 10.AUG.23 test_post3로 이동
+/*
+do it 만들기 1차 목표
+1. 체크박스 만들기
+2. 클릭하면 색 변화
+3. 상단에 '체크된 박스 수 / 만든 체크 박스 전체 수' 표시
+*/
 export default function Home() {
 
-
-
-
-
   const [count, setCount] = useState(0);
-
-
-  const onClick = () => {
-    setCount(count + 1);
-  };
-
-  const onClicks = () => {
-    setCount(count - 1);
-  };
 
   const Number = count.toString()
 
@@ -30,14 +23,10 @@ export default function Home() {
     color: "black"
   }
 
-  const clickAlert = () => {
-     alert("진짜?");
-  }
-
   return (
     <>
       <Head>
-        <title>Jey do it</title>
+        <title>Slow and steady wins the race</title>
       </Head>
       <>
 
@@ -45,70 +34,17 @@ export default function Home() {
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '30vh' }}>
 
           <h1 style={FontColor}>
-
-            Slow and steady wins the race.
-
+            Go Check
           </h1>
 
-          <h2 style={FontColor} >
-            <Link href="/posts/test_post">
-              click this!
-              <Image
-                src="/spk.png"
-                alt="Picture of the author"
-                width={100}
-                height={100}
-              />
-            </Link>
-          </h2>
-        </div>
-        <h1 style={FontColor} >
-          이렇게 하면<br></br> 어떻게 될까?
-          문단분리 안하면 그냥 쓰이는거고?
-          <p>
-            근데 이렇게 하면 문단 분리라는데 맞나 이거
-          </p>
-          여기 밑에 또 쓰면?
-        </h1>
-        <h2>
-          <Link href="/posts/test_post2">
-            <button>go button</button>
-          </Link>
-        </h2>
-
-        <div>
-          <button
-            onClick={onClick}
-          >+ 1 button</button>
-        </div>
-        <div>
-          <button
-            onClick={onClicks}
-          >- 1 button</button>
-        </div>
-        <div style ={FontColor}>
-          {Number}
-          {
-            Number === "7"
-              ? <p>아주 좋아요!</p>
-              : (Number === "19"
-                ? <p> 조금만 더!</p>
-                : <p> ??? </p>
-              )
-          }
-          {
-            Number === "20" && <Link href="/posts/test_post2"><button>드디어</button></Link>
-          }
-        </div>
-        <div>
-        <button onClick={clickAlert}>
-        arrow</button>
         </div>
         <label>
           <input type = "checkbox"></input>
+          <input type = "checkbox"></input>
+          <input type = "checkbox"></input>
+          <input type = "checkbox"></input>
+          <input type = "checkbox"></input>
         </label>
-
-
       </>
     </>
   )
