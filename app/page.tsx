ec6cfd8ddfa99fdcffcf1fb8b8240cdff7412e06
ad9306 <elyoon3004@gmail.com>
2023-08-26 05:22:28 +0900

    add addtask

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Link from 'next/link'
-
-
-const inter = Inter({ subsets: ['latin'] })
+import AddTask from './components/addtask'
 
 export default function Home() {
 
-  const FontColor = {
-    color: "black"
-  }
-
-  const BackSytle = {
-    backgroundColor : 'white'
-  }
-
-
   return (
     <>
-      <main>
+      <main className='max-w-4xl mx-auto mt-4'>
         <title>Slow and steady wins the race</title>
-
-
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '30vh'}}>
-
-          <h1 className='text-center underline'>
-            Go Check
-          </h1>
-          <button className='btn'>click me</button>
-        </div>
+          <div className='text-center my-5 flex flex-col gap-4'>
+            <h1 className='text-2xl font-bold'>Todo App</h1>
+            <AddTask />
+          </div>
       </main>
     </>
   )
