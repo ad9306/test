93153dc8aa7c6a84f3a8ec3c61b7f7268f63ee26
ad9306 <elyoon3004@gmail.com>
2023-08-26 05:06:42 +0900

    go tail

--- app/page.tsx
+++ app/page.tsx
@@ -3,49 +3,35 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
-import { useState } from 'react'
+
 
 const inter = Inter({ subsets: ['latin'] })
-//1차 정리 10.AUG.23 test_post3로 이동
-/*
-do it 만들기 1차 목표
-1. 체크박스 만들기
-2. 클릭하면 색 변화
-3. 상단에 '체크된 박스 수 / 만든 체크 박스 전체 수' 표시
-*/
+
 export default function Home() {
-
-  const [count, setCount] = useState(0);
-
-  const Number = count.toString()
 
   const FontColor = {
     color: "black"
   }
 
+  const BackSytle = {
+    backgroundColor : 'white'
+  }
+
+
   return (
     <>
-      <Head>
+      <main>
         <title>Slow and steady wins the race</title>
-      </Head>
-      <>
 
 
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '30vh' }}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '30vh'}}>
 
-          <h1 style={FontColor}>
+          <h1 className='text-center underline'>
             Go Check
           </h1>
-
+          <button className='btn'>click me</button>
         </div>
-        <label>
-          <input type = "checkbox"></input>
-          <input type = "checkbox"></input>
-          <input type = "checkbox"></input>
-          <input type = "checkbox"></input>
-          <input type = "checkbox"></input>
-        </label>
-      </>
+      </main>
     </>
   )
 }
