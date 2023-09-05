cf23d720ca7021ce26e006b4430b0a4f67603fa6
amek1004 <choihantae@me.com>
2023-09-06 05:55:31 +0900

    plus

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -1,22 +1,73 @@
-const TodoList = () => {
-    return <div className="overflow-x-auto">
-    <table className="table">
-      {/* head */}
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Favorite Color</th>
-        </tr>
-      </thead>
-      <tbody>
-        {/* row 1 */}
-        <tr>
-          <td>Cy Ganderton</td>
-          <td>Blue</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-}
+// const TodoList = () => {
+//     return <div className="overflow-x-auto">
+//     <table className="table">
+//       {/* head */}
+//       <thead>
+//         <tr>
+//           <th>Name</th>
+//           <th>Favorite Color</th>
+//         </tr>
+//       </thead>
+//       <tbody>
+//         {/* row 1 */}
+//         <tr>
+//           <td>Cy Ganderton</td>
+//           <td>Blue</td>
+//         </tr>
+//       </tbody>
+//     </table>
+//   </div>
+// }
 
-export default TodoList;
+// export default TodoList;
+"use client"
+
+import { useState } from "react"
+
+
+export default function TodoList() {
+
+
+  const [confirm, setConfirm] = useState(false)
+
+
+  console.log(confirm);
+
+
+
+  const todos = [
+    { id: 1, name: 'Buy Milk', completed: false },
+    { id: 2, name: 'Buy Eggs', completed: false },
+    { id: 3, name: 'Buy Bread', completed: false },
+    { id: 3, name: 'Buy Bread', completed: false },
+    { id: 3, name: 'Buy Bread', completed: false },
+
+  ]
+
+  return (
+    <div className="overflow-x-auto">
+      <table className="table">
+        {/* head */}
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Favorite Color</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {todos.map((todo) => (
+            <tr
+
+              key={todo.id}>
+              <td>{todo.name}</td>
+              <td>{todo.completed}</td>
+            </tr>
+          ))}
+        </tbody>
+
+      </table>
+    </div>
+  )
+
+}
--- app/components/addtask.tsx
+++ app/components/addtask.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { AiOutlinePlus } from 'react-icons/ai'
 /*
 아이콘은 react-icons에서 불러옴 아이콘별로 from이 달라지므로 확인하고 가져오기
@@ -5,8 +7,10 @@ import { AiOutlinePlus } from 'react-icons/ai'
 */
 const AddTask = () => {
     return <div>
-        <button className='btn btn-primary w-full'>Add new Task
-        <AiOutlinePlus className='ml-1' size={18}/></button>
+        <button
+            onClick={() => { console.log('clicked') }}
+            className='btn btn-primary w-full'>Add new Task
+            <AiOutlinePlus className='ml-1' size={18} /></button>
     </div>;
 };
 
