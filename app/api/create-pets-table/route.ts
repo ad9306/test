8629fdabd4d47a69015fc43db3fd3af7bcb2ee45
ad9306 <elyoon3004@gmail.com>
2023-09-16 23:09:21 +0900

    postgre

---
+++ app/api/create-pets-table/route.ts
@@ -0,0 +1,13 @@
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+ 
+export async function GET(request: Request) {
+  try {
+    const result =
+      await sql`CREATE TABLE IF NOT EXISTS Pets ( Name varchar(255), Owner varchar(255) );`;
+      const name = ['Daniel', 'Ben']
+    return NextResponse.json({ result }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
